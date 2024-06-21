50ee91680046e824edd160e7e4e2586050d6ef07
Kai Waldrant <kai@data-intuitive.com>
2024-06-21 16:57:53 +0200

    Update resources script

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -4,7 +4,12 @@ set -e
 
 echo ">> Downloading resources"
 
-viash run common/src/sync_resources/config.vsh.yaml -- \
+common/sync_resources/sync_resources \
   --input "s3://openproblems-data/resources_test/common/" \
-  --output "resources_test" \
+  --output "resources_test/common" \
+  --delete
+
+common/sync_resources/sync_resources \
+  --input "s3://openproblems-data/resources_test/denoising/" \
+  --output "resources_test/denoising" \
   --delete
