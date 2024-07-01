f071413c1888556a6bdf38c22439108f8d8b30a4
Kai Waldrant <kai@data-intuitive.com>
2024-07-01 16:49:51 +0200

    add fix to process_datasets

--- src/workflows/run_benchmark/run_test.sh
+++ src/workflows/run_benchmark/run_test.sh
@@ -17,13 +17,13 @@ fi
 
 export NXF_VER=22.04.5
 nextflow run . \
-  -main-script target/nextflow/denoising/workflows/run_benchmark/main.nf \
+  -main-script target/nextflow/workflows/run_benchmark/main.nf \
   -profile docker \
   -resume \
   -entry auto \
   -c common/nextflow_helpers/labels_ci.config \
   --input_states "$DATASETS_DIR/**/state.yaml" \
-  --rename_keys 'input_train:output_train,input_test:output_test' \
+  --rename_keys 'input_train:output_train;input_test:output_test' \
   --settings '{"output_scores": "scores.yaml", "output_dataset_info": "dataset_info.yaml", "output_method_configs": "method_configs.yaml", "output_metric_configs": "metric_configs.yaml", "output_task_info": "task_info.yaml"}' \
   --publish_dir "$OUTPUT_DIR" \
   --output_state "state.yaml"
